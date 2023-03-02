4d9687551dc360be171f0c92ea6048331be61d39
Nika-On <nikaonezashvili837@gmail.com>
2023-03-02 23:00:55 +0400

    fixed JS code

--- src/script.js
+++ src/script.js
@@ -1,24 +1,33 @@
-const expandSign = document.querySelectorAll(".expand-sign");
-const faqHeading = document.querySelectorAll(".faq-heading");
-const closeSign = document.querySelectorAll(".close-sign");
-const faqAnswer = document.querySelectorAll(".faq-answer");
-for (let i = 0; i < faqHeading.length; i++) {
-  faqHeading[i].addEventListener("click", () => {
-    if (faqAnswer[i].classList.contains("passive")) {
-      faqAnswer[i].classList.remove("passive");
-      expandSign[i].classList.add("passive");
-      closeSign[i].classList.remove("passive");
-    }else{
-      faqAnswer[i].classList.add("passive");
-      closeSign[i].classList.add("passive");
-      expandSign[i].classList.remove("passive");
+//Define variables.
+const expandSigns = document.querySelectorAll(".expand-sign");
+const faqHeadings = document.querySelectorAll(".faq-heading");
+const closeSigns = document.querySelectorAll(".close-sign");
+const faqAnswers = document.querySelectorAll(".faq-answer");
+// Add event listener to each faqHeading element
+faqHeadings.forEach((faqHeading, index) => {
+  faqHeading.addEventListener("click", () => {
+    // Get the classList of the next element sibling of the clicked faqHeading element.
+    let faqAnswer = faqHeading.nextElementSibling.classList;
+    // Expanding each faq-answer by adding "passive" class when faq-heading is clicked and closing faq-answer by removing "passive" class if same faq-heading is clicked again.
+    if (faqAnswer.contains("passive")) {
+      faqAnswer.remove("passive");
+      expandSigns[index].classList.add("passive");
+      closeSigns[index].classList.remove("passive");
+    } else {
+      faqAnswer.add("passive");
+      expandSigns[index].classList.remove("passive");
+      closeSigns[index].classList.add("passive");
     }
-    for (let j = 0; j < faqAnswer.length; j++) {
-      if (i !== j && !faqAnswer[j].classList.contains("passive")) {
-        faqAnswer[j].classList.add("passive");
-        closeSign[j].classList.add("passive");
-        expandSign[j].classList.remove("passive");
+    // Loop through all faqAnswer elements and add the "passive" class if they are already expanded.
+    faqAnswers.forEach((otherFaqAnswer, otherIndex) => {
+      if (
+        otherIndex !== index &&
+        !otherFaqAnswer.classList.contains("passive")
+      ) {
+        otherFaqAnswer.classList.add("passive");
+        closeSigns[otherIndex].classList.add("passive");
+        expandSigns[otherIndex].classList.remove("passive");
       }
-    }
+    });
   });
-}
+});
