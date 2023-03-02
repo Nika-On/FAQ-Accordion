4e368186c1d2961943721b52e7d05795c15c351d
Nika-On <nikaonezashvili837@gmail.com>
2023-03-02 21:51:09 +0400

    changed code

--- src/script.js
+++ src/script.js
@@ -1,24 +1,24 @@
 const expandSign = document.querySelectorAll(".expand-sign");
+const faqHeading = document.querySelectorAll(".faq-heading");
 const closeSign = document.querySelectorAll(".close-sign");
 const faqAnswer = document.querySelectorAll(".faq-answer");
-for (let i = 0; i < expandSign.length; i++) {
-  expandSign[i].addEventListener("click", () => {
-    faqAnswer[i].classList.remove("pasive");
-    closeSign[i].classList.remove("pasive");
-    expandSign[i].classList.add("pasive");
-    for(let j = 0; j < faqAnswer.length;j++){
-        if(i !== j && !(faqAnswer[j].classList.contains("pasive"))){
-            faqAnswer[j].classList.add("pasive");
-            closeSign[j].classList.add("pasive");
-            expandSign[j].classList.remove("pasive");
-        }
+for (let i = 0; i < faqHeading.length; i++) {
+  faqHeading[i].addEventListener("click", () => {
+    if (faqAnswer[i].classList.contains("passive")) {
+      faqAnswer[i].classList.remove("passive");
+      expandSign[i].classList.add("passive");
+      closeSign[i].classList.remove("passive");
+    }else{
+      faqAnswer[i].classList.add("passive");
+      closeSign[i].classList.add("passive");
+      expandSign[i].classList.remove("passive");
     }
-  });
-}
-for (let i = 0; i < closeSign.length; i++) {
-  closeSign[i].addEventListener("click", () => {
-    faqAnswer[i].classList.add("pasive");
-    closeSign[i].classList.add("pasive");
-    expandSign[i].classList.remove("pasive");
+    for (let j = 0; j < faqAnswer.length; j++) {
+      if (i !== j && !faqAnswer[j].classList.contains("passive")) {
+        faqAnswer[j].classList.add("passive");
+        closeSign[j].classList.add("passive");
+        expandSign[j].classList.remove("passive");
+      }
+    }
   });
 }
